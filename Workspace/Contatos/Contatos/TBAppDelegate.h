034d3a7d1f075dfa19dcbbac0b9716538b118084
Tiarê Balbi <tiarebalbi@me.com>
2014-08-09 15:14:04 -0300

    Initial Commit

---
+++ Workspace/Contatos/Contatos/TBAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TBAppDelegate.h
+//  Contatos
+//
+//  Created by ios4584 on 09/08/14.
+//  Copyright (c) 2014 Tiarê Balbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Workspace/Contatos/Contatos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contatos
+//
+//  Created by ios4584 on 09/08/14.
+//  Copyright (c) 2014 Tiarê Balbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBAppDelegate class]));
+    }
+}
